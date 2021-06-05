2603454eb5034361ae00cbd6fdbdf993e150ebaf
harryherzberg <harryherzberg@gmail.com>
2021-06-05 15:16:56 -0700

    Update test_calculator.py
    adding multiply and divide

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
+        
+    def test_multiply(self):
+        assert 2 == calculator.multiply(1, 2)
+
+    def test_divide(self):
+        assert 2 == calculator.divide(10, 5)
